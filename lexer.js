0e9466c086f45081524dfa50f8d24a26f86c350a
Ajay Patnaik <patnaikaj@gmail.com>
2020-05-01 00:06:07 -0400

    added all tokens to lexer

--- lexer.js
+++ lexer.js
@@ -1,11 +1,13 @@
 const isAssignment = token => /([:+\-*/]=)|\+\+|--/.test(token)
-const isBool = token => /#(TRUE)|(FALSE)/.test(token)
+const isBool = token => /(TRUE)|(FALSE)/.test(token)
 const isBooleanOperator = token => /([<>]=)|==/.test(token)
 const isNumericalOperator = token => /[+\-*/]/.test(token)
 
 const isSpace = token => /[ ]+/.test(token)
 const isIdentifier = token => /[_a-zA-Z][_a-zA-Z0-9]*/.test(token)
 const isNumber = token => /[0-9]+/.test(token)
+
+const isValidCharacter = token => /[_a-zA-Z0-9(){}+-\/*<> !]|(:=)/.test(token)
 
 function lex(input) {
 	let characters = input.split("")
@@ -16,42 +18,120 @@ function lex(input) {
 	for (let index = 0; index < characters.length; index++) {
 		currentToken += characters[index]
 		console.log(currentToken)
-		console.log(isIdentifier(currentToken))
 
-		// fix if there are no spaces in between identifier and token
-
-		if(currentToken == "+") {
-			if((index + 1) != characters.length && characters[index + 1] == "="){
-				tokens.push("PLUSEQUAL")
+		if(isSpace(characters[index+1]) || (index + 1) == characters.length || (!isIdentifier(characters[index+1]) && !isNumber(characters[index+1]))) {
+			if(currentToken == "if" || currentToken == "IF") {
+				tokens.push("IF")
 				currentToken = ""
-			} else {
+			} else if(currentToken == "else" || currentToken == "ELSE") {
+				tokens.push("ELSE")
+				currentToken = ""
+			} else if(currentToken == "while" || currentToken == "WHILE") {
+				tokens.push("WHILE")
+				currentToken = ""
+			} else if(currentToken == "function" || currentToken == "FUNCTION") {
+				tokens.push("FUNCTION")
+				currentToken = ""
+			} else if(currentToken == "TRUE") {
+				tokens.push("TRUE")
+				currentToken = ""
+			} else if(currentToken == "FALSE") {
+				tokens.push("FALSE")
+				currentToken = ""
+			} else if(isIdentifier(currentToken)) {
+				tokens.push("IDENTIFIER:" + currentToken.trim())
+				currentToken = ""
+			} else if(isNumber(currentToken)) {
+				tokens.push("NUMBER:" + currentToken.trim())
+				currentToken = ""
+			} 
+		}
+
+		if(isSpace(currentToken)) currentToken = ""
+
+		if(currentToken == ":=") {
+			tokens.push("EQUALS")
+			currentToken = ""
+		} else if(currentToken == "==") {
+			tokens.push("EQUALSEQUALS")
+			currentToken = ""
+		} else if(currentToken == "!=") {
+			tokens.push("NOTEQUALS")
+			currentToken = ""
+		} else if(currentToken == "+") {
+			if(characters[index+1] != "=" && characters[index+1] != "+") {
 				tokens.push("PLUS")
 				currentToken = ""
 			}
-		} else if(currentToken == ":=") {
-			tokens.push("EQUAL")
+		} else if(currentToken == "+=") {
+			tokens.push("PLUSEQUALS")
 			currentToken = ""
-		}else if(currentToken == "(") {
+		} else if(currentToken == "++") {
+			tokens.push("PLUSPLUS")
+			currentToken = ""
+		} else if(currentToken == "-") {
+			if(characters[index+1] != "=" && characters[index+1] != "-") {
+				tokens.push("MINUS")
+				currentToken = ""
+			}
+		} else if(currentToken == "-=") {
+			tokens.push("MINUSEQUALS")
+			currentToken = ""
+		} else if(currentToken == "--") {
+			tokens.push("MINUSMINUS")
+			currentToken = ""
+		} else if(currentToken == "*") {
+			if(characters[index+1] != "=") {
+				tokens.push("TIMES")
+				currentToken = ""
+			}
+		} else if(currentToken == "*=") {
+			tokens.push("TIMESEQUALS")
+			currentToken = ""
+		} else if(currentToken == "/") {
+			if(characters[index+1] != "=") {
+				tokens.push("DIV")
+				currentToken = ""
+			}
+		} else if(currentToken == "/=") {
+			tokens.push("DIVEQUALS")
+			currentToken = ""
+		} else if(currentToken == ">") {
+			if(characters[index+1] != "=") {
+				tokens.push("GREATER")
+				currentToken = ""
+			}
+		} else if(currentToken == ">=") {
+			tokens.push("GREATEREQUALS")
+			currentToken = ""
+		} else if(currentToken == "<") {
+			if(characters[index+1] != "=") {
+				tokens.push("LESSER")
+				currentToken = ""
+			}
+		} else if(currentToken == "<=") {
+			tokens.push("LESSEREQUALS")
+			currentToken = ""
+		} else if(currentToken == "(") {
 			tokens.push("LPAREN")
+			currentToken = ""
+		} else if(currentToken == ")") {
+			tokens.push("RPAREN")
 			currentToken = ""
 		} else if(currentToken == "{") {
 			tokens.push("LBRACE")
 			currentToken = ""
-		} else if(characters[index] == " " || (index + 1) == characters.length) {
-			if(isIdentifier(currentToken)) {
-				tokens.push("IDENTIFIER:" + currentToken.trim())
-				currentToken = ""
-			} else if(isNumber(currentToken)) {
-				// TODO determine type of number here
-				tokens.push("NUMBER:" + currentToken.trim())
-				currentToken = ""
-			} else {
+		} else if(currentToken == "}") {
+			tokens.push("RBRACE")
+			currentToken = ""
+		} else {
+			if(!isValidCharacter(characters[index]) && isValidCharacter(characters[index+1])) {
+				tokens.push("INVALID:" + currentToken)
 				currentToken = ""
 			}
 		}
-
-
-		
 	}
+
 	console.log(tokens)
+	return tokens
 }
