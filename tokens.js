4bfcea067cb92605e342b8873971735ae396a0cf
Jacob Schwartz <jtschwartz98@gmail.com>
2020-05-03 11:19:49 -0400

    Start to possible parser. Trying Chevrotain on new branch

--- tokens.js
+++ tokens.js
@@ -8,25 +8,29 @@ class Token {
 	get() {
 		return this._value
 	}
+	
+	set(value) {
+		this._value = value
+	}
 }
 
 class Assignment extends Token {
 	
 	constructor(op) {
-		if (!(op in [":=", "+=", "-=", "*=", "/=", "++", "--"])) throw "Assignment: Not a valid assignment type."
+		if (!(op in ["EQUALS", "PLUSEQUALS", "MINUSEQUALS", "TIMESEQUALS", "DIVEQUALS", "PLUS+", "MINUS-"])) throw "Assignment: Not a valid assignment type."
 		
 		super(op)
 	}
 	
 	operation(identifier, value = null) {
 		switch (this._value) {
-			case ":=":   identifier.set(value); break
-			case "+=":  identifier.set(identifier.get + value); break
-			case "-=":  identifier.set(identifier.get - value); break
-			case "*=":  identifier.set(identifier.get * value); break
-			case "/=":  identifier.set(identifier.get / value); break
-			case "++":  identifier.set(identifier.get + 1); break
-			case "--":  identifier.set(identifier.get - 1); break
+			case "EQUALS":   identifier.set(value); break
+			case "PLUSEQUALS":  identifier.set(identifier.get + value); break
+			case "MINUSEQUALS":  identifier.set(identifier.get - value); break
+			case "TIMESEQUALS":  identifier.set(identifier.get * value); break
+			case "DIVEQUALS":  identifier.set(identifier.get / value); break
+			case "PLUSPLUS":  identifier.set(identifier.get + 1); break
+			case "MINUSMINUS":  identifier.set(identifier.get - 1); break
 		}
 	}
 }
@@ -34,12 +38,12 @@ class Assignment extends Token {
 class Bool extends Token {
 	
 	constructor(val) {
-		if (val !== "#TRUE" && val !== "#FALSE") throw "Boolean: Not a valid boolean value"
-		super(val);
+		if (val !== "TRUE" && val !== "FALSE") throw "Boolean: Not a valid boolean value"
+		super(val === "TRUE");
 	}
 	
 	get() {
-		return this._value === "#TRUE";
+		return this._value;
 	}
 	
 	toNum() {
@@ -50,21 +54,16 @@ class Bool extends Token {
 class Num extends Token {
 	
 	constructor(val) {
-		const parsed = Number
 		if (isNaN(Number(val))) throw "Number: Not a valid number value"
 		
 		super(Number(val))
-	}
-	
-	set(val) {
-		this._value = val
 	}
 }
 
 class Operator extends Token {
 	
 	constructor(op) {
-		if (!(op in ["+", "-", "*", "/", ">", ">=", "<", "<="])) throw "Operation: Not a valid operation type."
+		if (!(op in ["PLUS", "MINUS", "TIMES", "DIV", "GREATER", "GREATEREQUALS", "LESSER", "LESSEREQUALS"])) throw "Operation: Not a valid operation type."
 		
 		super(op)
 	}
@@ -73,21 +72,21 @@ class Operator extends Token {
 		let value;
 		
 		switch (this._value) {
-			case "+":   value = a + b; break
-			case "-":   value = a - b; break
-			case "*":   value = a * b; break
-			case "/":   value = a / b; break
-			case ">":   value = a > b; break
-			case ">=":  value = a >= b; break
-			case "<":   value = a < b; break
-			case "<=":  value = a <= b; break
+			case "PLUS":   value = a + b; break
+			case "MINUS":   value = a - b; break
+			case "TIMES":   value = a * b; break
+			case "DIV":   value = a / b; break
+			case "GREATER":   value = a > b; break
+			case "GREATEREQUALS":  value = a >= b; break
+			case "LESSER":   value = a < b; break
+			case "LESSEREQUALS":  value = a <= b; break
 		}
 	}
 }
 
 class BooleanOperator extends Operator{
 	constructor(op) {
-		if (!(op in [">", ">=", "<", "<=", "==", "!="])) throw "Operation: Not a valid operation type."
+		if (!(op in ["GREATER", "GREATEREQUALS", "LESSER", "LESSEREQUALS", "EQUALSEQUALS", "NOTEQUALS"])) throw "Operation: Not a valid operation type."
 		
 		super(op)
 	}
@@ -96,12 +95,12 @@ class BooleanOperator extends Operator{
 		let value;
 		
 		switch (this._value) {
-			case ">":   value = a > b; break
-			case ">=":  value = a >= b; break
-			case "<":   value = a < b; break
-			case "<=":  value = a <= b; break
-			case "==":  value = a === b; break
-			case "!=":  value = a !== b; break
+			case "GREATER":   value = a > b; break
+			case "GREATEREQUALS":  value = a >= b; break
+			case "LESSER":   value = a < b; break
+			case "LESSEREQUALS":  value = a <= b; break
+			case "EQUALSEQUALS":  value = a === b; break
+			case "NOTEQUALS":  value = a !== b; break
 		}
 		
 		return new Bool(value ? "#TRUE" : "#FALSE")
@@ -110,17 +109,33 @@ class BooleanOperator extends Operator{
 
 class NumericalOperator extends Operator{
 	constructor(op) {
-		if (!(op in ["+", "-", "*", "/"])) throw "Operation: Not a valid operation type."
+		if (!(op in ["PLUS", "MINUS", "TIMES", "DIV"])) throw "Operation: Not a valid operation type."
 		
 		super(op)
 	}
 	
 	operation(a, b) {
 		switch (this._value) {
-			case "+":   return new Num(a + b)
-			case "-":   return new Num(a - b)
-			case "*":   return new Num(a * b)
-			case "/":   return new Num(a / b)
+			case "PLUS":   return new Num(a + b)
+			case "MINUS":   return new Num(a - b)
+			case "TIMES":   return new Num(a * b)
+			case "DIV":   return new Num(a / b)
 		}
+	}
+}
+
+class Identifier extends Token {
+	_assignment = null
+	
+	constructor(name) {
+		super(name);
+	}
+	
+	get() {
+		return this._assignment
+	}
+	
+	set(value) {
+		this._assignment = value
 	}
 }
