29095cf3857c8c9c122848223324e57f9f6d382c
Jacob Schwartz <jtschwartz98@gmail.com>
2020-05-07 19:38:29 -0400

    Reformatting

--- executor.js
+++ executor.js
@@ -191,7 +191,7 @@ class Parser extends CstParser {
 				{ALT: () => $.CONSUME(Num)}
 			])
 		})
-
+		
 		$.RULE("printOp", () => {
 			$.CONSUME(Print)
 			$.CONSUME(Identifier)
@@ -291,6 +291,7 @@ const LanguageParser = new Parser()
 
 // Switch to without Defaults once all visitor methods have been implemented
 const BaseCstVisitor = LanguageParser.getBaseCstVisitorConstructor()
+
 // const BaseCstVisitor = LanguageParser.getBaseCstVisitorConstructorWithDefaults()
 
 class Interpreter extends BaseCstVisitor {
@@ -330,51 +331,51 @@ class Interpreter extends BaseCstVisitor {
 	expr(ctx) {
 		if (ctx.Num) {
 			return Number(ctx.Num[0].image)
-		} else if(ctx.assignment) {
+		} else if (ctx.assignment) {
 			return this.visit(ctx.assignment)
-		} else if(ctx.mathExpr) {
+		} else if (ctx.mathExpr) {
 			return this.visit(ctx.mathExpr)
-		} else if(ctx.condition) {
+		} else if (ctx.condition) {
 			return this.visit(ctx.condition)
-		} else if(ctx.unaryExpr) {
+		} else if (ctx.unaryExpr) {
 			return this.visit(ctx.unaryExpr)
-		} else if(ctx.ifOp) {
+		} else if (ctx.ifOp) {
 			return this.visit(ctx.ifOp)
-		} else if(ctx.whileOp) {
+		} else if (ctx.whileOp) {
 			return this.visit(ctx.whileOp)
-		} else if(ctx.printOp) {
+		} else if (ctx.printOp) {
 			return this.visit(ctx.printOp)
-		} else if(ctx.func) {
+		} else if (ctx.func) {
 			return this.visit(ctx.func)
-		}else if(ctx.funcCall) {
+		} else if (ctx.funcCall) {
 			return this.visit(ctx.funcCall)
 		}
 	}
-
+	
 	condition(ctx) {
-		if(ctx.bool) {
+		if (ctx.bool) {
 			const val = this.visit(ctx.bool)
-			if(tokenMatcher(val, True)) {
+			if (tokenMatcher(val, True)) {
 				return true
-			} else if(tokenMatcher(val, False)) {
+			} else if (tokenMatcher(val, False)) {
 				return false
 			}
-		} else if(ctx.comparison) {
+		} else if (ctx.comparison) {
 			return this.visit(ctx.comparison)
 		}
 	}
-
+	
 	comparison(ctx) {
-		var num1 = 0
-		var num2 = 0
-
-		if(ctx.Identifier) {
-			//TODO: error handling
-
+		let num1 = 0
+		let num2 = 0
+		
+		if (ctx.Identifier) {
+			if (!(ctx.Identifier[0].image in identifiers)) throw "Identifier needs to be initialized before comparison"
+			
 			const id1 = ctx.Identifier[0].image
 			num1 = identifiers[id1]
-
-			if(ctx.Identifier[1]) {
+			
+			if (ctx.Identifier[1]) {
 				const id2 = ctx.Identifier[1].image
 				num2 = identifiers[id2]
 			} else if (ctx.Num) {
@@ -388,26 +389,26 @@ class Interpreter extends BaseCstVisitor {
 		}
 		
 		const op = this.visit(ctx.boolOp)
-
-		if(tokenMatcher(op, Greater)) {
+		
+		if (tokenMatcher(op, Greater)) {
 			return num1 > num2
-		} else if(tokenMatcher(op, GreaterEquals)) {
+		} else if (tokenMatcher(op, GreaterEquals)) {
 			return num1 >= num2
-		} else if(tokenMatcher(op, Lesser)) {
+		} else if (tokenMatcher(op, Lesser)) {
 			return num1 < num2
-		} else if(tokenMatcher(op, LesserEqual)) {
+		} else if (tokenMatcher(op, LesserEqual)) {
 			return num1 <= num2
-		} else if(tokenMatcher(op, Equals)) {
+		} else if (tokenMatcher(op, Equals)) {
 			return num1 === num2
-		} else if(tokenMatcher(op, NotEquals)) {
+		} else if (tokenMatcher(op, NotEquals)) {
 			return num1 !== num2
 		}
 	}
-
+	
 	bool(ctx) {
-		if(ctx.True) {
+		if (ctx.True) {
 			return ctx.True[0].tokenType
-		} else if(ctx.False) {
+		} else if (ctx.False) {
 			return ctx.False[0].tokenType
 		}
 	}
@@ -416,7 +417,6 @@ class Interpreter extends BaseCstVisitor {
 		const name = ctx.id[0].image
 		const parameters = ctx.Identifier.map(x => x.image)
 		identifiers[name] = {params: parameters, pgm: JSON.parse(JSON.stringify(ctx.declaration[0]))}
-		
 		
 		
 		return "function"
@@ -447,7 +447,7 @@ class Interpreter extends BaseCstVisitor {
 	
 	parameters(ctx) {
 		let all = []
-
+		
 		if (ctx.Num) {
 			all.push(...ctx.Num)
 		}
@@ -463,9 +463,9 @@ class Interpreter extends BaseCstVisitor {
 		if (ctx.Identifier) {
 			all.push(...ctx.Identifier)
 		}
-
+		
 		all = all.sort((a, b) => a.startOffset - b.startOffset)
-
+		
 		all = all.map(token => {
 			if (tokenMatcher(token.tokenType, Identifier)) {
 				return identifiers[token.image]
@@ -480,46 +480,46 @@ class Interpreter extends BaseCstVisitor {
 		
 		return all
 	}
-
+	
 	numOp(ctx) {
-		if(ctx.Add) {
+		if (ctx.Add) {
 			return ctx.Add[0].tokenType
-		} else if(ctx.Subtract) {
+		} else if (ctx.Subtract) {
 			return ctx.Subtract[0].tokenType
-		} else if(ctx.Multiply) {
+		} else if (ctx.Multiply) {
 			return ctx.Multiply[0].tokenType
-		} else if(ctx.Divide) {
+		} else if (ctx.Divide) {
 			return ctx.Divide[0].tokenType
 		}
 	}
-
+	
 	boolOp(ctx) {
-		if(ctx.Greater) {
+		if (ctx.Greater) {
 			return ctx.Greater[0].tokenType
-		} else if(ctx.GreaterEquals) {
+		} else if (ctx.GreaterEquals) {
 			return ctx.GreaterEquals[0].tokenType
-		} else if(ctx.Lesser) {
+		} else if (ctx.Lesser) {
 			return ctx.Lesser[0].tokenType
-		} else if(ctx.LesserEqual) {
+		} else if (ctx.LesserEqual) {
 			return ctx.LesserEqual[0].tokenType
-		} else if(ctx.Equals) {
+		} else if (ctx.Equals) {
 			return ctx.Equals[0].tokenType
-		} else if(ctx.NotEquals) {
+		} else if (ctx.NotEquals) {
 			return ctx.NotEquals[0].tokenType
 		}
 	}
-
+	
 	mathExpr(ctx) {
-		var num1 = 0
-		var num2 = 0
-
-		if(ctx.Identifier) {
-			//TODO: error handling
-
+		let num1 = 0
+		let num2 = 0
+		
+		if (ctx.Identifier) {
+			if (!(ctx.Identifier[0].image in identifiers)) throw "Identifier needs to be initialized before computation"
+			
 			const id1 = ctx.Identifier[0].image
 			num1 = identifiers[id1]
-
-			if(ctx.Identifier[1]) {
+			
+			if (ctx.Identifier[1]) {
 				const id2 = ctx.Identifier[1].image
 				num2 = identifiers[id2]
 			} else {
@@ -529,78 +529,71 @@ class Interpreter extends BaseCstVisitor {
 			num1 = Number(ctx.Num[0].image)
 			num2 = Number(ctx.Num[1].image)
 		}
-
+		
 		const op = this.visit(ctx.numOp)
-
-		if(tokenMatcher(op, Add)) {
+		
+		if (tokenMatcher(op, Add)) {
 			return num1 + num2
-		} else if(tokenMatcher(op, Subtract)) {
+		} else if (tokenMatcher(op, Subtract)) {
 			return num1 - num2
-		} else if(tokenMatcher(op, Multiply)) {
+		} else if (tokenMatcher(op, Multiply)) {
 			return num1 * num2
-		} else if(tokenMatcher(op, Divide)) {
+		} else if (tokenMatcher(op, Divide)) {
 			return num1 / num2
 		}
 	}
-
+	
 	unaryExpr(ctx) {
-		var val = undefined
-		var id = undefined
-
-		if(ctx.Identifier) {
-			// TODO: handle error if id does not exist in identifiers
-
+		let val = undefined
+		let id = undefined
+		
+		if (ctx.Identifier) {
+			if (!(ctx.Identifier[0].image in identifiers)) throw "Identifier needs to be initialized before computation"
+			
 			id = ctx.Identifier[0].image
 			val = identifiers[id]
 		} else {
 			val = Number(ctx.Num[0].image)
 		}
-
+		
 		const op = this.visit(ctx.unaryOp)
-
-		if(tokenMatcher(op, AddOne)) {
+		
+		if (tokenMatcher(op, AddOne)) {
 			identifiers[id] = val + 1
 			return val + 1
-		} else if(tokenMatcher(op, SubtractOne)) {
+		} else if (tokenMatcher(op, SubtractOne)) {
 			identifiers[id] = val - 1
 			return val - 1
 		}
 	}
-
+	
 	unaryOp(ctx) {
-		if(ctx.AddOne) {
+		if (ctx.AddOne) {
 			return ctx.AddOne[0].tokenType
-		} else if(ctx.SubOne) {
+		} else if (ctx.SubOne) {
 			return ctx.SubOne[0].tokenType
 		}
 	}
 	
 	ifOp(ctx) {
 		const condition = this.visit(ctx.condition)
-		const expr = this.visit(ctx.program)
-		let elseExpr = undefined
-
-		if(ctx.elseExpr) { elseExpr = this.visit(ctx.elseExpr) }
-
-		if(condition) {
-			return expr
-		} else {
-			if(elseExpr) {
-				return elseExpr
-			} else {
-				return undefined
-			}
+		
+		if (condition) {
+			this.visit(ctx.program)
+		} else if (ctx.elseExpr) {
+			this.visit(ctx.elseExpr)
 		}
+		
 	}
-
+	
 	printOp(ctx) {
 		const id = ctx.Identifier[0].image
 		console.log(`=> ${identifiers[id]}`)
 	}
-
+	
 	// while (val < 4) {val++}
 	whileOp(ctx) {
-		while(this.visit(ctx.condition)) {
+		while (this.visit(ctx.condition)) {
 			this.visit(ctx.program)
 		}
 	}
